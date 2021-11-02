6556af88e18f16bb5cd634e5667f304e0066f692
62050217 <87146400+62050217@users.noreply.github.com>
2021-11-02 14:41:20 +0700

    Update sum_ints_client.py

--- sum_ints_client.py
+++ sum_ints_client.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 
 import sys
 import rospy
-from beginner_tutorials.srv import *
+from rattapong_topic.srv import *
 
 def add_two_ints_client(x, y):
     rospy.wait_for_service('add_two_ints')
